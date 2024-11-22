1b1b3e4971b4deda7258617f88f2f143f3deae9b
AYUSHSAHU2004 <141218241+AYUSHSAHU2004@users.noreply.github.com>
2024-11-23 00:56:45 +0530

    added CompleteProblems

--- backend/Routes/problemRoutes.js
+++ backend/Routes/problemRoutes.js
@@ -11,7 +11,19 @@ router.get('/getProblem/:email',ProblemController.getProblemEmail);
 router.get('/getProblem/:location/:department',ProblemController.getProblemDepartment);
 router.get('/getProblemDetail/:id',ProblemController.getProblemDetail);
 router.post('/updateProblem',upload.single('imageUrl'),ProblemController.updateProblem);
-router.delete('DeleteProblem/:id',ProblemController.DeleteProblem);
+router.delete('/DeleteProblem/:id',ProblemController.DeleteProblem);
+
+
+
+router.post('/createCompleteProblem',ProblemController.createCompleteProblem);
+router.get('/getAllCompleteProblem',ProblemController.getAllCompleteProblem);
+router.get('/getAllCompleteProblem/:location',ProblemController.getCompleteProblemLocation);
+router.get('/getCompleteProblem/:email',ProblemController.getCompleteProblemEmail);
+router.get('/getCompleteProblem/:location/:department',ProblemController.getCompleteProblemDepartment);
+router.get('/getCompleteProblemDetail/:id',ProblemController.getCompleteProblemDetail);
+
+
+
 
 // Exporting the router to be used in the main server file
 module.exports = router;
