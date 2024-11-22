516bc08fd3cd6f8f9ca562c5cd897164b083e18a
its-bismay <2023ugee070@nitjsr.ac.in>
2024-11-22 23:42:53 +0530

    dynamic navigation

---
+++ front/src/Pages/UserDashboard/UserDashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PostIssue from '../../components/PostIssue'
+
+const UserDashboard = () => {
+  return (
+    <div>UserDashboard
+        <PostIssue />
+    </div>
+
+  )
+}
+
+export default UserDashboard
