63a6dedcc76c4340203789a999cf8e8ba7d587a6
its-bismay <2023ugee070@nitjsr.ac.in>
2024-11-23 01:12:22 +0530

    User Dashboard updated

--- front/src/components/PostIssue.jsx
+++ front/src/components/PostIssue.jsx
@@ -1,48 +1,80 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const PostIssue = () => {
+    const email = localStorage.getItem("user");
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [formData, setFormData] = useState({
+      email: "",
+      location: "",
+      tags: "",
+      imageUrl: "",
+      publicCheck:"",
+      authorityCheck:"",
+      problemDetail:""
+    });
 
-    const sampleData = [
-        {
-          id: 1,
-          imageUrl: 'https://images.pexels.com/photos/25652584/pexels-photo-25652584/free-photo-of-elegant-man-wearing-navy-suit.jpeg?auto=compress&cs=tinysrgb&w=400',
-          category: 'Sample Category 1',
-          description: 'Sample Description 1'
-        },
-        {
-          id: 2,
-          imageUrl: 'https://images.pexels.com/photos/25652584/pexels-photo-25652584/free-photo-of-elegant-man-wearing-navy-suit.jpeg?auto=compress&cs=tinysrgb&w=400',
-          category: 'Sample Category 2',
-          description: 'Sample Description 2'
-        },
-        {
-          id: 3,
-          imageUrl: 'https://images.pexels.com/photos/25652584/pexels-photo-25652584/free-photo-of-elegant-man-wearing-navy-suit.jpeg?auto=compress&cs=tinysrgb&w=400',
-          category: 'Sample Category 3',
-          description: 'Sample Description 3'
+    useEffect(() => {
+      // Function to fetch data
+      const url = `http://localhost:5000/problems/getProblem/${email}`
+      const fetchData = async () => {
+        try {
+          const response = await axios.get(url); // Replace with your API endpoint
+          setData(response.data);
+          setLoading(false);
+        } catch (err) {
+          setError(err.message);
+          setLoading(false);
         }
-      ];
+      };
+  
+      // Fetch data on component mount
+      fetchData();
+    }, []);
+
+
+    if (loading){return <p>Loading...</p>} else {
+      console.log(data)
+    }
+    if (error) return <p>Error: {error}</p>;
+
+
+
+
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error: {error}</p>;
+
+      const handleAuthorityInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevState) => ({
+          ...prevState,
+          [name]: value,
+        }));
+      };
       
   return (
     <div class="bg-gray-100">
-        <div class="header my-3 h-12 px-10 flex items-center justify-between">
-            <h1 class="font-medium text-2xl">Post an issue</h1>
-        </div>
         <div class="flex flex-col mx-3 mt-6 lg:flex-row">
             <div class="w-full lg:w-1/3 m-1">
                 <form class="w-full bg-white shadow-md p-6">
                     <div class="flex flex-wrap -mx-3 mb-6">
                         <div class="w-full md:w-full px-3 mb-6">
                             <label class="block uppercase tracking-wide text-gray-700 text-sm font-bold mb-2" htmlFor="category_name">Problem Tag</label>
-                            <input class="appearance-none block w-full bg-white text-gray-900 font-medium border border-gray-400 rounded-lg py-3 px-3 leading-tight focus:outline-none focus:border-[#98c01d]" type="text" name="name" placeholder="Category Name" required />
+                            <input
+                                name="tags"
+                                value={formData.tags}
+                                onChange={handleAuthorityInputChange}
+                                class="appearance-none block w-full bg-white text-gray-900 font-medium border border-rounded-lg py-3 px-3 leading-tight focus:outline-none focus:border-[#98c01d]" type="text" placeholder="Tag Department" required />
                         </div>
                         <div class="w-full md:w-full px-3 mb-6">
                             <label class="block uppercase tracking-wide text-gray-700 text-sm font-bold mb-2" htmlFor="category_name">Location</label>
-                            <input class="appearance-none block w-full bg-white text-gray-900 font-medium border border-gray-400 rounded-lg py-3 px-3 leading-tight focus:outline-none focus:border-[#98c01d]" type="text" name="name" placeholder="Category Name" required />
+                            <input value={formData.location} onChange={handleAuthorityInputChange} class="appearance-none block w-full bg-white text-gray-900 font-medium border border-gray-400 rounded-lg py-3 px-3 leading-tight focus:outline-none focus:border-[#98c01d]" type="text" name="location" placeholder="Enter Location" required />
                         </div>
                         <div class="w-full px-3 mb-6">
                         <label class="block uppercase tracking-wide text-gray-700 text-sm font-bold mb-2" htmlFor="category_name">Problem Description</label>
-                            <textarea textarea rows="4" class="appearance-none block w-full bg-white text-gray-900 font-medium border border-gray-400 rounded-lg py-3 px-3 leading-tight focus:outline-none focus:border-[#98c01d]" type="text" name="description" required > </textarea>
+                            <textarea value={formData.problemDetail} onChange={handleAuthorityInputChange} textarea rows="4" class="appearance-none block w-full bg-white text-gray-900 font-medium border border-gray-400 rounded-lg py-3 px-3 leading-tight focus:outline-none focus:border-[#98c01d]" type="text" name="problemDetail" required> </textarea>
                         </div>                        
                         
 
@@ -53,7 +85,7 @@ const PostIssue = () => {
                             <path strokeLinecap="round" strokeLinejoin="round" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
                             </svg>
 
-                            <h2 class="mt-4 text-xl font-medium text-gray-700 tracking-wide">Category image</h2>
+                            <h2 class="mt-4 text-xl font-medium text-gray-700 tracking-wide">Image Proofs</h2>
 
                             <p class="mt-2 text-gray-500 tracking-wide">Upload or drag & drop your file SVG, PNG, JPG or GIF. </p>
 
@@ -63,7 +95,7 @@ const PostIssue = () => {
 
                         <div class="w-full md:w-full px-3 mb-6">
                             <button class="appearance-none block w-full bg-green-700 text-gray-100 font-bold border border-gray-200 rounded-lg py-3 px-3 leading-tight hover:bg-green-600 focus:outline-none focus:bg-white focus:border-gray-500"
-                            >Add Category</button>
+                            >Post Problem</button>
                         </div>
                         
                     </div>
@@ -74,28 +106,22 @@ const PostIssue = () => {
         <table className="table-auto w-full">
           <thead className="text-sm font-semibold uppercase text-gray-800 bg-gray-50 mx-auto">
             <tr>
-              <th className="p-2">#</th>
-              <th className="p-2">Problem</th>
-              <th className="p-2">Description</th>
+              <th className="p-2 text-left">#</th>
+              <th className="p-2 text-left">Tag</th>
+              <th className="p-2 text-left">Description</th>
               <th className="p-2 text-center">Action</th>
             </tr>
           </thead>
           <tbody>
-            {sampleData.map((item, index) => (
-              <tr key={index}>
-                <td className="p-2">{index + 1}</td>
-                <td className="p-2"><img src={item.imageUrl} alt="Problem" className="h-8 w-8 mx-auto" /></td>
-                <td className="p-2">{item.category}</td>
-                <td className="p-2">{item.description}</td>
+            {data.map((item, index) => (
+              <tr key={index} className="border-t border-gray-200">
+                <td className="p-2 text-left">{index + 1}</td>
+                <td className="p-2 text-left">{item.tags}</td>
+                <td className="p-2 text-left">{item.problemDetail}</td>
                 <td className="p-2 text-center">
-                  <div className="flex justify-center">
-                    <a href="#" className="rounded-md hover:bg-green-100 text-green-600 p-2 flex justify-between items-center">
-                      <span><h1 className="w-4 h-4 mr-1"></h1></span> Edit
-                    </a>
-                    <button className="rounded-md hover:bg-red-100 text-red-600 p-2 flex justify-between items-center">
-                      <span><h1 className="w-4 h-4 mr-1"></h1></span> Delete
-                    </button>
-                  </div>
+                  <button className="rounded-md bg-blue-500 text-white p-2 hover:bg-blue-700">
+                    Update
+                  </button>
                 </td>
               </tr>
             ))}
